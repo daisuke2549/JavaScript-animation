aee61dbd42062357afaf490c2f0e73530cc0004b
佐々木大輔 <daisuke.sasaki@onsuku.jp>
2021-05-25 18:03:53 +0900

    complete coding JS pop-up-animation

--- click-pop-up/main.js
+++ click-pop-up/main.js
@@ -1,10 +1,10 @@
 function popupImage() {
-    var popup = document.getElementById('js-popup');
-    if(!popup) return;
+    let popup = document.getElementById('js-popup');
+    if(!popup) return;   //js-popupというidを取得したときにjs-black-bg、js-close-btn,js-show-popupという３つのidを取得
   
-    var blackBg = document.getElementById('js-black-bg');
-    var closeBtn = document.getElementById('js-close-btn');
-    var showBtn = document.getElementById('js-show-popup');
+    let blackBg = document.getElementById('js-black-bg');
+    let closeBtn = document.getElementById('js-close-btn');
+    let showBtn = document.getElementById('js-show-popup');
   
     closePopUp(blackBg);
     closePopUp(closeBtn);
@@ -12,8 +12,15 @@ function popupImage() {
     function closePopUp(elem) {
       if(!elem) return;
       elem.addEventListener('click', function() {
-        popup.classList.toggle('is-show');
+        popup.classList.toggle('is-show'); //toggleでcis-show classのaddとremoveを実施
       });
     }
   }
   popupImage();
+
+
+const a = 3;
+
+const b = 2;
+
+console.log((a> 0 || b > 0 ));
