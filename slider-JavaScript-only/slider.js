4327ddafbb4a42db064fe585db62cc8e23b3f1bd
Daisuke Sasaki <gerund25495641@gmail.com>
2021-05-26 07:59:14 +0900

    add slider animation code

---
+++ slider-JavaScript-only/slider.js
@@ -0,0 +1,22 @@
+// let slider = document.getElementById('sliderArea');
+// let activeImageIndex = 0;
+
+// let imageArray = slider.getElementsByTagName('img');
+// imageArray[activeImageIndex].style.display = 'block';
+
+// function nextImage(){
+//     imageArray[activeImageIndex].style.display   ='none';
+//     if(activeImageIndex >= imageArray.length -1){    // 配列の最後の要素の場合、indexは0を取得
+//         activeImageIndex  = 0;
+//     }else{
+//         activeImageIndex += 1;  
+//       }
+//     imageArray[activeImageIndex].style.display = 'block';
+// }
+
+
+// const array = [1,2,3,4,5];
+
+
+// console.log(array.length -1);
+// console.log(array[4]);
