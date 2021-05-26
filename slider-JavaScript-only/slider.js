91a4f09018ec804015350680dafcc1611090b744
Daisuke Sasaki <gerund25495641@gmail.com>
2021-05-26 10:27:10 +0900

    スライダーアニメーション実装

--- slider-JavaScript-only/slider.js
+++ slider-JavaScript-only/slider.js
@@ -1,22 +1,18 @@
-// let slider = document.getElementById('sliderArea');
-// let activeImageIndex = 0;
-
-// let imageArray = slider.getElementsByTagName('img');
-// imageArray[activeImageIndex].style.display = 'block';
-
-// function nextImage(){
-//     imageArray[activeImageIndex].style.display   ='none';
-//     if(activeImageIndex >= imageArray.length -1){    // 配列の最後の要素の場合、indexは0を取得
-//         activeImageIndex  = 0;
-//     }else{
-//         activeImageIndex += 1;  
-//       }
-//     imageArray[activeImageIndex].style.display = 'block';
-// }
-
-
-// const array = [1,2,3,4,5];
-
-
-// console.log(array.length -1);
-// console.log(array[4]);
+var mySwiper = new Swiper('.swiper-container', {
+    autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	},
+    speed: 1000,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	}
+});
